46bd4619e854949a46a668ec891f8b02287f89d5
Olivier Brinkman <ollybrinkman@gmail.com>
2022-11-20 21:14:53 +0100

    Sharing - Contact - Pay, UXUI HOT FIXes

--- src/Helpers/ETHContracts.js
+++ src/Helpers/ETHContracts.js
@@ -1,122 +1,115 @@
 export const ethContracts = {
-    chain: "eth",
-    tokens: [
-      {
-        name: "Ethereum ",
-        symbol: "ETH",
-        icon:"https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/eth.svg",
-        contract: "Native",
-        contract_5:"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-        isActive: false,
-        color: "#627EEA"
+  chain: "eth",
+  tokens: [{
+          name: "Ethereum ",
+          symbol: "ETH",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/eth.svg",
+          contract: "Native",
+          contract_5: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+          isActive: false,
+          color: "#627EEA"
       },
       {
-        name: "USD Coin",
-        symbol: "USDC",
-        icon:
-          "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
-          "cc06e86182bf2c55a76bbc/svg/color/usdc.svg",
-        contract: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-        contract_5: "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C",
-        decimals: 6,
-        isActive: true,
-        color: "#3E73C4"
+          name: "USD Coin",
+          symbol: "USDC",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
+              "cc06e86182bf2c55a76bbc/svg/color/usdc.svg",
+          contract: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+          contract_5: "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C",
+          decimals: 6,
+          isActive: true,
+          color: "#3E73C4"
 
       },
       {
-        name: "Tether",
-        symbol: "USDT",
-        icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/usdt.svg",
-        contract: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-        contract_5:"0xe802376580c10fe23f027e1e19ed9d54d4c9311e",
-        decimals: 6,
-        isActive: true,
-        color: "#92D0BD"
+          name: "Tether",
+          symbol: "USDT",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/usdt.svg",
+          contract: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+          contract_5: "0xe802376580c10fe23f027e1e19ed9d54d4c9311e",
+          decimals: 6,
+          isActive: true,
+          color: "#92D0BD"
 
       },
-  
-      {
-        name: "Dai Stablecoin",
-        symbol: "DAI",
-        icon:
-          "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
-          "cc06e86182bf2c55a76bbc/svg/color/dai.svg",
-        contract: "0x6b175474e89094c44da98b954eedeac495271d0f",
-        contract_5:"0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60",
-        decimals: 18,
-        isActive: true,
-        color: "#F4B731"
 
-      },
       {
-        name: "Binance USD",
-        symbol: "BUSD",
-        icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/busd.svg",
-        contract: "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
-        contract_5:"0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-        decimals: 18,
-        isActive: true,
-        color: "#2A5ADA"
-      },  
-      {
-        name: "Binance Coin",
-        symbol: "BNB",
-        icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/bnb.svg",
-        contract: "0xc96eb356c6c3e748dc6663dec9bb571c3a6d1c37",
-        contract_5:"0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-        decimals: 18,
-        isActive: true,
-        color: "#2A5ADA"
-      },
-	    {
-        name: "Cronos",
-        symbol: "CRO",
-        icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/cro.svg",
-        contract: "0xA0b73E1Ff0B80914AB6fe0444E65848C4C34450b",
-        contract_5:"0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-        decimals: 8,
-        isActive: true,
-        color: "#2A5ADA"
-      },
-      {
-        name: "Basic Attention Token",
-        symbol: "BAT",
-        icon:
-          "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
-          "cc06e86182bf2c55a76bbc/svg/color/bat.svg",
-        contract: "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
-        contract_5: "0x70cBa46d2e933030E2f274AE58c951C800548AeF",
-        decimals: 18,
-        isActive: true,
-        color: "#FF5000"
+          name: "Dai Stablecoin",
+          symbol: "DAI",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
+              "cc06e86182bf2c55a76bbc/svg/color/dai.svg",
+          contract: "0x6b175474e89094c44da98b954eedeac495271d0f",
+          contract_5: "0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60",
+          decimals: 18,
+          isActive: true,
+          color: "#F4B731"
 
       },
       {
-        name: "Chainlink",
-        symbol: "LINK",
-        icon:
-          "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
-          "cc06e86182bf2c55a76bbc/svg/color/link.svg",
-        contract: "0x514910771af9ca656af840dff83e8264ecf986ca",
-        contract_5:"0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-        decimals: 18,
-        isActive: true,
-        color: "#2A5ADA"
+          name: "Binance USD",
+          symbol: "BUSD",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/busd.svg",
+          contract: "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
+          contract_5: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+          decimals: 18,
+          isActive: true,
+          color: "#2A5ADA"
+      },
+      {
+          name: "Binance Coin",
+          symbol: "BNB",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/bnb.svg",
+          contract: "0xc96eb356c6c3e748dc6663dec9bb571c3a6d1c37",
+          contract_5: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+          decimals: 18,
+          isActive: true,
+          color: "#2A5ADA"
+      },
+      {
+          name: "Cronos",
+          symbol: "CRO",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/cro.svg",
+          contract: "0xA0b73E1Ff0B80914AB6fe0444E65848C4C34450b",
+          contract_5: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+          decimals: 8,
+          isActive: true,
+          color: "#2A5ADA"
+      },
+      {
+          name: "Basic Attention Token",
+          symbol: "BAT",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
+              "cc06e86182bf2c55a76bbc/svg/color/bat.svg",
+          contract: "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+          contract_5: "0x70cBa46d2e933030E2f274AE58c951C800548AeF",
+          decimals: 18,
+          isActive: true,
+          color: "#FF5000"
 
       },
       {
-        name: "Polygon ",
-        symbol: "MATIC",
-        icon:
-          "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
-          "cc06e86182bf2c55a76bbc/svg/color/matic.svg",
-        contract: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
-        contract_5:"0x8A6bE4f8ca7B3B5aA1935b0C5de1801fDe1134d9",
-        decimals: 18,
-        isActive: true,
-        color: "#6F41D8"
+          name: "Chainlink",
+          symbol: "LINK",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
+              "cc06e86182bf2c55a76bbc/svg/color/link.svg",
+          contract: "0x514910771af9ca656af840dff83e8264ecf986ca",
+          contract_5: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
+          decimals: 18,
+          isActive: true,
+          color: "#2A5ADA"
 
       },
-    ],
-  };
-  
+      {
+          name: "Polygon ",
+          symbol: "MATIC",
+          icon: "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169d" +
+              "cc06e86182bf2c55a76bbc/svg/color/matic.svg",
+          contract: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+          contract_5: "0x8A6bE4f8ca7B3B5aA1935b0C5de1801fDe1134d9",
+          decimals: 18,
+          isActive: true,
+          color: "#6F41D8"
+
+      },
+  ],
+};
